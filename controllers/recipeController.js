b41ed42c6c1554188efb158bbb24eed35f55b97b
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2024-04-25 19:06:11 -0700

    Recipes controller and route  done and stable

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -5,8 +5,23 @@ export async function getAllRecipes(req, res) {
     res.json(data);
 };
 
+export async function getRecipe(req, res) {
+    const recipe = await Recipe.findById(req.params.id);
+    res.json(recipe);
+};
+
 export async function addRecipe(req, res) {
     const recipe = new Recipe(req.body);
     await recipe.save();
-    res.json(recipe);
+    res.json({ "message": "Recipe created successfully", "recipe": recipe});
+};
+
+export async function updateRecipe(req, res) {
+    const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json({ "message": "Recipe updated successfully", "recipe": recipe });
+};
+
+export async function deleteRecipe(req, res) {
+    await Recipe.findByIdAndDelete(req.params.id);
+    res.json({ "message": "Recipe deleted successfully" });
 };
